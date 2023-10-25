2d2598d82d46ccf4c9e62aa39e1d6a640bcc48f8
Rkaly567 <148215698+Rkaly567@users.noreply.github.com>
2023-10-25 18:44:56 +0100

    Add files via upload

--- Player.py
+++ Player.py
@@ -2,18 +2,27 @@ import pygame
 
 class Player:
     def __init__(self, x, y, screen_height):
-        img = pygame.image.load('img/guy1.png')
-        self.image = pygame.transform.scale(img, (40, 80))
+        self.images_right = [
+            pygame.transform.scale(pygame.image.load('img/guy1.png'), (40, 80)),
+            pygame.transform.scale(pygame.image.load('img/guy2.png'), (40, 80)),
+            pygame.transform.scale(pygame.image.load('img/guy3.png'), (40, 80)),
+            pygame.transform.scale(pygame.image.load('img/guy4.png'), (40, 80))
+        ]
+
+        self.counter = 0
+        self.index = 0
+        self.image = self.images_right[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
         self.screen_height = screen_height
-
+    
     def update(self):
         dx = 0
         dy = 0
+        walk_cd = 5
 
         # get keypresses
         key = pygame.key.get_pressed()
@@ -23,9 +32,20 @@ class Player:
         if not key[pygame.K_SPACE]:
             self.jumped = False
         if key[pygame.K_LEFT]:
+            self.counter += 1
             dx -= 5
         if key[pygame.K_RIGHT]:
             dx += 5
+            self.counter += 1
+                
+        #Animation
+        if self.counter > walk_cd:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_right):
+                self.index = 0
+            self.image = self.images_right[self.index]
+            
 
         # add gravity
         self.vel_y += 1
