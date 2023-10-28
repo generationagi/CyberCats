b5d916987501efc0e6a7d05b6ab555bb99a21daa
gocryptodomain <101332023+gocryptodomain@users.noreply.github.com>
2023-10-28 20:43:04 +0100

    little adjustments
     adjusted the way the score variable is updated when the player collects mushrooms and also reset it when the game state changes. also added a call to pygame.spritecollide to check for collisions between the player and mushrooms, and the score is displayed on the screen.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame import mixer
 from pygame.locals import *
 from Player import Player
 from World import World  
@@ -36,7 +35,6 @@ bg_img = pygame.image.load('img/sky1.png')
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x,y))
-	
 
 #load sounds
 pygame.mixer.music.load('sound/music.mp3')
@@ -47,14 +45,6 @@ jump_fx = pygame.mixer.Sound('sound/jump.wav')
 jump_fx.set_volume(0.5)
 game_over_fx = pygame.mixer.Sound('sound/death.wav')
 game_over_fx.set_volume(0.5)
-
-
-
-def draw_grid():
-	for line in range(0, 20):
-		pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-		pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))          
-
 
 def reset_game():
     # Reset player's position
@@ -68,9 +58,7 @@ quit_button = Button(screen_width // 2 + 100, screen_height // 16, 'img/quit.png
 world = World(world_data, tile_size)
 
 blob_group = world.blob_group
-Lava_group = pygame.sprite.Group()
 lava_group = world.lava_group
-mushroom_group = pygame.sprite.Group()
 mushroom_group = world.mushroom_group
 exit_group = world.exit_group
 
@@ -94,8 +82,6 @@ while run:
                 elif quit_button.rect.collidepoint(event.pos):
                     quit_button.quit_game()
 
-
-    #Rendering
     if player.game_state == 1:
         start_button.draw(screen)
         quit_button.draw(screen)
@@ -110,22 +96,26 @@ while run:
         blob_group.update()
         player.update()
         player.draw(screen)
+        score = 0  # Reset the score
         mushroom_group.draw(screen)
         mushroom_group.update()
-	if pygame.sprite.spritecollide(player, mushroom_group, True):
-		score += 1
-	draw_text("X " + str(score), font_score, red, tile_size -10, 10)
-         
+        
+        if pygame.sprite.spritecollide(player, mushroom_group, True):
+            score += 1
+        
+        draw_text("X " + str(score), font_score, red, tile_size - 10, 10)
+        
     if player.game_state == -1:
         player.image = player.death_image
         player.update()
         player.draw(screen)
         if start_button.draw(screen):
-                reset_game()
+            reset_game()
         start_button.draw(screen)
         quit_button.draw(screen)
-	score = 0
+        score = 0  # Reset the score
 
     pygame.display.update()
 
 pygame.quit()
+
